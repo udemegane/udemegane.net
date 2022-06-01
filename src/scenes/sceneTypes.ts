4458962b4fab3f417c90b8b36d9b508e001d6d31
udemegane <cappuccino1204.ck@gmail.com>
2022-06-02 01:53:28 +0900

    いらないのを削除

--- src/scenes/sceneTypes.ts
+++ src/scenes/sceneTypes.ts
@@ -1,12 +1,5 @@
-import { Scene } from "@babylonjs/core";
-
 export const SceneType = {
   Title: 0,
 } as const;
 // eslint-disable-next-line no-redeclare
 export type SceneType = typeof SceneType[keyof typeof SceneType];
-
-export type TaggedScene = {
-  tag: SceneType;
-  scene: Scene;
-};
